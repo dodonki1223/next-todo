75a862f6ab29004b0b3cc8b4a107237cdbc19619
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-09-02 09:54:26 +0900

    :sparkles: useState を使用し、カウントアップするボタンを追加する

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react';
 import styles from '../styles/Home.module.css'
 
 const Home = () => {
-  return <div style={{ margin: '64px'}}>Home</div>
+  const [count, setCount] = useState(0);
+  const handleAddClick = () => {
+    setCount((prevCount) => prevCount + 1)
+  };
+
+  return (
+    <div style={{ margin: '64px'}}>
+      Home
+    <div>{count}</div>
+    <button type='button' onClick={handleAddClick}>
+      Add!
+    </button>
+    </div>
+  )
 }
 
 export default Home

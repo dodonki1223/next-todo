30b88091eb8db4ace3f299f2ae18afef4baaf1a5
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-09-02 09:59:28 +0900

    :sparkles: useEffect を使って count が変更されるたびに実行される処理を追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,23 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from '../styles/Home.module.css'
 
 const Home = () => {
   const [count, setCount] = useState(0);
+  const [displayName, setDisplayName] = useState('');
   const handleAddClick = () => {
     setCount((prevCount) => prevCount + 1)
   };
+
+  // count が変更されるたびに useEffect が実行されます
+  useEffect(() => {
+    if (count !== 0 && count %3 === 0) {
+      setDisplayName('3の倍数！！！');
+    } else {
+      setDisplayName('');
+    }
+  }, [count])
 
   return (
     <div style={{ margin: '64px'}}>
@@ -16,6 +26,7 @@ const Home = () => {
     <button type='button' onClick={handleAddClick}>
       Add!
     </button>
+    <h2>{displayName}</h2>
     </div>
   )
 }
